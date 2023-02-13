ebe6ab46ec4f15a1fb0c3b54bf08af522467cc32
idkhow2type <lecaominhhn@gmail.com>
2023-02-13 16:17:03 +0700

    new front page test, package json stuff for glitch

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import fs from 'fs';
 
+const app = express();
+
 for (const dir of fs.readdirSync('pages')) {
     for (const file of fs.readdirSync(`pages/${dir}`)) {
-        console.log(file);
         if (file.endsWith('.html'))
             fs.copyFileSync(`pages/${dir}/${file}`, `views/${file}`);
         else if (file.endsWith('.css'))
@@ -12,27 +13,24 @@ for (const dir of fs.readdirSync('pages')) {
             fs.copyFileSync(`pages/${dir}/${file}`, `scripts/${file}`);
     }
 }
-// const app = express();
-// const port = 3000;
 
-// app.engine('html', (path, options, callback) => {
-//     fs.readFile(path, (err, content) => {
-//         if (err) return callback(err);
-//         const rendered = content.toString().replace(/{{(\w+)}}/g, (_, p1) => {
-//             return options[p1];
-//         });
-//         return callback(null, rendered);
-//     });
-// });
+app.engine('html', (path, options, callback) => {
+    fs.readFile(path, (err, content) => {
+        if (err) return callback(err);
+        const rendered = content.toString().replace(/{{(\w+)}}/g, (_, p1) => {
+            return options[p1];
+        });
+        return callback(null, rendered);
+    });
+});
 
-// app.use(express.static('public'));
-// app.set('views', './views');
-// app.set('view engine', 'html');
+app.use(express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'html');
 
-// app.get('/', (req, res) => {
-//     if (req.query.action === 'new') res.redirect(`/${tick('', 'new').grid}`);
-//     else res.render('index');
-// });
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 // app.get('/:grid', (req, res) => {
 //     // this is kinda eww but idfc
@@ -50,6 +48,7 @@ for (const dir of fs.readdirSync('pages')) {
 //         });
 // });
 
-// app.listen(port, () => {
-//     console.log(`Listening on port ${port}`);
-// });
+const port = 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
