6e8b68b526818450f059c70c360dc1d27961cfd1
idkhow2type <lecaominhhn@gmail.com>
2023-02-15 09:12:37 +0700

    idfk i did a lot

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ import fs from 'fs';
 
 const app = express();
 
+for (const dir of ['public', 'scripts', 'views']) {
+    try {
+        if (!fs.existsSync()) fs.mkdirSync(dir);
+    } catch (e) {}
+}
 for (const dir of fs.readdirSync('pages')) {
     for (const file of fs.readdirSync(`pages/${dir}`)) {
+        if (!fs.existsSync(`public/${dir}`)) fs.mkdirSync(`public/${dir}`);
         if (file.endsWith('.html'))
             fs.copyFileSync(`pages/${dir}/${file}`, `views/${file}`);
-        else if (file.endsWith('.css'))
-            fs.copyFileSync(`pages/${dir}/${file}`, `public/${file}`);
         else if (file.endsWith('.js'))
             fs.copyFileSync(`pages/${dir}/${file}`, `scripts/${file}`);
+        else if (['.css', '.png'].some((s) => file.endsWith(s)))
+            fs.copyFileSync(`pages/${dir}/${file}`, `public/${dir}/${file}`);
     }
+}
+
+const modules = {};
+for (const file of fs.readdirSync('scripts')) {
+    modules[file.slice(0, file.length - 3)] = await import(`./scripts/${file}`);
 }
 
 app.engine('html', (path, options, callback) => {
@@ -29,26 +40,31 @@ app.set('views', './views');
 app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        games: fs
+            .readdirSync('pages')
+            .filter((dir) => !['index'].includes(dir))
+            .map(
+                (dir) =>
+                    `<li><a href="/${dir}"><div class="image-cover"></div><div class="image" style="--url: url('/${dir}/thumbnail.png')"></div><h2>${dir}</h2></a></li>`
+            )
+            .join(''),
+        page: 'index',
+    });
 });
 
-// app.get('/:grid', (req, res) => {
-//     // this is kinda eww but idfc
-//     if (req.query.action) {
-//         const state = tick(req.params.grid, req.query.action);
-//         res.redirect(`/${state.grid}?gameOver=${state.gameOver}`);
-//     } else
-//         res.render('game', {
-//             grid: render(req.params.grid),
-//             gridData: req.params.grid,
-//             gameOver:
-//                 req.query.gameOver === 'true'
-//                     ? '<div class="game-over">Game over!</div>'
-//                     : '',
-//         });
-// });
+app.get('/favicon.ico', (req, res) => res.status(204));
 
-const port = 3000
+app.get('/:game', (req, res) => {
+    console.log(req.params);
+    modules[req.params.game].start(req, res);
+});
+
+app.get('/:game/:data', (req, res) => {
+    modules[req.params.game].update(req, res);
+});
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
